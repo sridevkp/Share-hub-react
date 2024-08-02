a7d0f2786ca77c3520ae9687adab9914a3f5c5d9
sridev <69075577+sridevkp@users.noreply.github.com>
2024-08-02 16:42:09 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const PORT = 8080 ;
 const httpServer = http.createServer();
 const io = new Server(httpServer, {
   cors :{
-    origin: "http://localhost:5173",
+    origin: "https://share-hub-eight.vercel.app",
     methods: ["GET","POST"]
   }
 });
